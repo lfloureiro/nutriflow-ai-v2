c5ae44ceacd8bc749f4dc88dd1082b01b3b90796
Luis Loureiro <156515052+lfloureiro@users.noreply.github.com>
2026-08-21 15:37:54 +0100

    Register nutrition constraint model

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.anthropometric_measurement import AnthropometricMeasurement
 from app.models.family import Family
+from app.models.nutrition_constraint import NutritionConstraint
 from app.models.nutrition_goal import NutritionGoal
 from app.models.person import Person
 from app.models.person_profile import PersonProfile
@@ -7,6 +8,7 @@ from app.models.person_profile import PersonProfile
 __all__ = [
     "AnthropometricMeasurement",
     "Family",
+    "NutritionConstraint",
     "NutritionGoal",
     "Person",
     "PersonProfile",
