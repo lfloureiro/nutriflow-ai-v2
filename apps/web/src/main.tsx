4f7fbd27f12efe1ef0ca2af9bf42b3350d3fec80
Luis Loureiro <156515052+lfloureiro@users.noreply.github.com>
2026-08-24 19:36:56 +0100

    Load delivery menu styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -11,6 +11,7 @@ import "./family-meals.css";
 import "./family-person-setup.css";
 import "./family-settings.css";
 import "./ingredient-catalogue.css";
+import "./delivery-menu.css";
 import "./core-planning.css";
 import "./pantry-shopping.css";
 import "./recommendation-planner.css";
